f915d051a08362ab76c8500b21fcfce75e4fa176
Certil Remy <certil@noukod.com>
2019-01-08 21:25:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,13 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   url = ["name", "hometown", "favorite-song"]
 
-  url.each { |link|
+  get '/' do
+      "Hello World"
+    end
 
-    get '/link' do
-      
-  end
-    
   }
 
-  
+
 end
