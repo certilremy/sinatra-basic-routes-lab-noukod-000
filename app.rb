f9d5f6d0fde505a8e5b1ff680cfade454b485c1d
Certil Remy <certil@noukod.com>
2019-01-08 21:38:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Certil Remy"
+  end
+
+  get '/hometown' do
+    "My hometown is Port-au-Prince"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Zelda"
+  end
+
 end
